283c93a0c1dce463b342e61ba9312bafc81f8600
AroSebastine <48196046+AroSebastine@users.noreply.github.com>
2022-10-09 12:08:25 +1030

    Add files via upload

---
+++ Task3AvgOf5Numbers08Oct.java
@@ -0,0 +1,16 @@
+package assignments;
+
+public class Task3AvgOf5Numbers08Oct {
+
+	public static void main(String[] args) {
+		float a = 10;
+		float b = 90.78f;
+		float c = 111;
+		float d = 8989;
+		float e = 7876;
+		// 10,90.78,111,8989,7876
+		
+		System.out.println("Average = "+(a+b+c+d+e)/5);
+	}
+
+}
---
+++ Task5PrintOddNumbers08Oct.java
@@ -0,0 +1,15 @@
+package assignments;
+
+public class Task5PrintOddNumbers08Oct {
+
+	public static void main(String[] args) {
+		
+		for (int i=1;i<=200;i++) {
+			
+			if (i%2 != 0) {
+				System.out.println(i);			
+			}			
+		}
+	}
+
+}
